ef40310dc28695c470c5f7b6742bbe6f595e0bb8
spy0008 <namansompura2@gmail.com>
2025-06-23 15:23:40 +0530

    Implement about section complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -96,7 +96,6 @@ const Hero = () => {
                     muted
                     playsInline
                     preload="auto"
-                    autoPlay
                 />
             </div>
         </>
