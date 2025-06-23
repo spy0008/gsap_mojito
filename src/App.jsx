a4302426f8889d0f7523d8f8a5ab174c0e7135b1
spy0008 <namansompura2@gmail.com>
2025-06-23 19:16:40 +0530

    Implement contact section complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
 import About from './components/About'
 import Art from './components/Art'
+import Manu from './components/Manu'
+import Contact from './components/Contact'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -12,10 +14,12 @@ const App = () => {
   return (
     <main>
       <NavBar />
-      <Hero/>
-      <Cocktails/>
-      <About/>
-      <Art/>
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Manu />
+      <Contact />
     </main>
   )
 }
